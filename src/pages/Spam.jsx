5b6810e917d6098f1ca71f6642b50cb20d4bb37b
Pranita <pfulsundar8@gmail.com>
2023-04-26 23:06:59 +0530

    refactor: each page and functionality

--- src/pages/Spam.jsx
+++ src/pages/Spam.jsx
@@ -1,17 +1,1 @@
-import { useContext } from "react";
-import { MailContext } from "..";
-import MailCard from "../components/MailCard";
 
-const Spam = () => {
-  const { state } = useContext(MailContext);
-  return (
-    <>
-      <h2>Spam</h2>
-      {state.spamMails.map((email) => (
-          <MailCard key={email.mId} email={email} isAddedToSpam />
-      ))}
-    </>
-  );
-};
-
-export default Spam;
--- src/pages/Trash.jsx
+++ src/pages/Trash.jsx
@@ -1,19 +1,1 @@
-import { useContext } from "react";
-import MailCard from "../components/MailCard";
-import { MailContext } from "../contexts/MailContext";
 
-const Trash = () => {
-  const { state } = useContext(MailContext);
-  return (
-    <>
-      <h2>Trash</h2>
-      <div>
-        {state.trashMails.map((mail) => (
-            <MailCard key={mail.mId} email={mail} isAddedToTrash/>
-        ))}
-      </div>
-    </>
-  );
-};
-
-export default Trash;
